e96ee6c21a0b33bb12602174d3ea57bb04ee70d6
Admin <sahilkudake7@gmail.com>
2020-03-03 12:21:34 +0530

    code updated

--- Springboot/src/main/java/com/bridgelabz/springboot/SpringbootApplication.java
+++ Springboot/src/main/java/com/bridgelabz/springboot/SpringbootApplication.java
@@ -3,6 +3,12 @@ package com.bridgelabz.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Purpose : Program to perform crud operation using spring boot
+ * 
+ * @author Sahil Kudake
+ *
+ */
 @SpringBootApplication
 public class SpringbootApplication {
 
